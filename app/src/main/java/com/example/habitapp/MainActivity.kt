8df06fae7d6523acb74818aad29e438ef9910465
trackofficial <tsikhenkoviktorr@gmail.com>
2026-02-28 19:21:23 +0300

    initial commit

---
+++ app/src/main/java/com/example/habitapp/MainActivity.kt
@@ -0,0 +1,14 @@
+
+package com.example.habitapp
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_screen)
+
+    }
+}
