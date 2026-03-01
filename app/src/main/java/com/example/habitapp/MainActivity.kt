a8f6aebbaae21c7afbe0c40eba5e4ae231bc2207
trackofficial <tsikhenkoviktorr@gmail.com>
2026-03-01 14:54:13 +0300

    style: 1. add icon 2. add background

--- app/src/main/java/com/example/habitapp/MainActivity.kt
+++ app/src/main/java/com/example/habitapp/MainActivity.kt
@@ -4,7 +4,7 @@ package com.example.habitapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-
+    
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
